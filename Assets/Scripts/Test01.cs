c094a1d2aec65326977fde7237b83cd945f069d4
kconcon3 <hkane3sub@excite.co.jp>
2018-11-09 13:48:45 +0900

    first commit

---
+++ Assets/Scripts/Test01.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+#if UNITY_EDITOR
+		// AnimatorControllerを生成
+		CreateAnimatorControllerInheritance createAnimCtrl = new CreateAnimatorControllerInheritance();
+		createAnimCtrl.Init();
+#endif
+	}
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# UnityCreateAnimatorControllerTest01
+## 概要  
+UnityのAnimatorControllerをスクリプトから生成してみる  
+<br /> 
+## テストした時の環境  
+Windows7 Pro 64bit  
+Unity 2018.2.10f1  
+Visual Studio Community 2017  
+
+
